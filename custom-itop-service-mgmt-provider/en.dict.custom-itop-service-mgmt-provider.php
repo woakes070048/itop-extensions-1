610880d27e0f3aebb02e2031469e51dd2998c8db
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-03-10 14:08:14 +0100

    Show new table in tab and fix translations

--- custom-itop-service-mgmt-provider/en.dict.custom-itop-service-mgmt-provider.php
+++ custom-itop-service-mgmt-provider/en.dict.custom-itop-service-mgmt-provider.php
@@ -6,7 +6,31 @@
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
+//
+// Class: CustomerContract
+//
 Dict::Add('EN US', 'English', 'English', array(
     'Class:CustomerContract/Attribute:location_id' => 'Location',
+    'Class:CustomerContract/Attribute:cimodelpricing_list' => 'CI Model Pricing',
+    'Class:CustomerContract/Attribute:cimodelpricing_list+' => '',
+));
+
+//
+// Class: lnkCustomerContractToCIModelPricing
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+    'Class:lnkCustomerContractToCIModelPricing' => 'Link Customer Contract / CI Model Pricing',
+    'Class:lnkCustomerContractToCIModelPricing+' => '',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:customercontract_id' => 'Customer Contact',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:customercontract_id+' => '',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:customercontract_name' => 'Name',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:customercontract_name+' => '',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:ci_model_id' => 'CI Model',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:ci_model_id+' => '',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:ci_model_name' => 'CI Model Name',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:ci_model_name+' => '',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:price' => 'Price',
+    'Class:lnkCustomerContractToCIModelPricing/Attribute:price+' => '',
 ));
 ?>
